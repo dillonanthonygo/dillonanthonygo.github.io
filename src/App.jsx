d4bf15986c845761bb558d6063304aa81bd27c01
dillonanthonygo <dillonanthonygo@gmail.com>
2025-10-16 22:04:45 -0700

    navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import "./App.css"
 import { LoadingScreen } from "./components/LoadingScreen"
 import "./index.css"
 import { Navbar } from "./components/Navbar"
+import { MobileMenu } from "./components/MobileMenu"
 
 function App() {
-  const [isLoaded, setisLoaded] = useState(false)
+  const [isLoaded, setisLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setisLoaded(true)}/>}{" "}
         <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-white text-black-100`}>
-          <Navbar />
+          <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
 
         </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
-export const Navbar = () => {
+import { useEffect } from "react";
+
+export const Navbar = ({menuOpen, setMenuOpen}) => {
+
+    useEffect(() => {
+        document.body.style.overflow = menuOpen ? "hidden" : "";
+    }, [menuOpen]);
+
     return (
         <nav className="fixed top-0 w-full z-40 bg-[rgba(255,219,220,.8)] backdrop-blue-lg border-b border-black/10 shadow-lg">
             <div className="max-2-5xl mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
                     <a href="#home" className="font-mono text-xl font-bold text-black"> logo </a>
+                
+                {/* Mobile */}
+                <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
+                    &#9776;
+                </div>
+
+                {/* Desktop */}
+                <div className="hidden md:flex items-center space-x-8">
+                    <a href="#home" className="text-black-300 hove:text-black transtion-colors"> Home </a>
+                    <a href="#about" className="text-black-300 hove:text-black transtion-colors"> About </a>
+                    <a href="#projects" className="text-black-300 hove:text-black transtion-colors"> Projects </a>
+                    <a href="#contact" className="text-black-300 hove:text-black transtion-colors"> Contact </a>
+                </div>
                 </div>
             </div>
         </nav>
